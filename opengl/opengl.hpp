42fe71f0a729649fce5577a5e901851319611837
lyrahgames <lyrahgames@mailbox.org>
2022-06-26 18:56:17 +0200

    Separate shader object and program

--- opengl/opengl.hpp
+++ opengl/opengl.hpp
@@ -2,5 +2,6 @@
 //
 #include <opengl/buffer.hpp>
 #include <opengl/camera.hpp>
-#include <opengl/shader.hpp>
+#include <opengl/shader_object.hpp>
+#include <opengl/shader_program.hpp>
 #include <opengl/vertex_array.hpp>
